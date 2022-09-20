43cff8467703caaeb72bbec59ccdbac332d37503
Melanson <Lee.Melanson@LibertyMutual.com>
2022-09-20 14:45:04 -0400

    populate state dropdown by json file

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,5 @@
+    
+    
     async function getParks() {
         let url = 'https://uptacamp.github.io/session_two/assets/scripts/data/national_parks.json';
         try {
@@ -10,13 +12,33 @@
     }
     
     async function displayParks() {
-        let parks_result = await getParks();
+        let parks_result = await getParks();      
+        let state_list = [];
+        let dropdown = $('#by_location');
         
+        dropdown.empty();
+        dropdown.append('<option selected="true" disabled>Choose State</option>');
+        dropdown.prop('selectedIndex', 0);
+        
+        //cycle through
         for (let i = 0; i < parks_result.parks.length; i++){
-            console.log(parks_result.parks[i].Address);
+            current_state = parks_result.parks[i].State;
+            
+            //populate location drop down
+
+        if (state_list.indexOf(current_state) > -1 ){
+            //if already in the list, do nothing
+        } else
+        {
+            //if not in the list, add it to array
+            state_list.push(current_state);
+            
+            dropdown.append($('<option></option>').attr('value', parks_result.parks[i].State).text(parks_result.parks[i].State));
         }
-        
-        //return parks_result.parks[0].Address;
+         
+        }
+        console.log(state_list);
+       
         }
     
         
