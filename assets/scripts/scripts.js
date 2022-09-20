af7e31fa8999a265decdd8f07734b909bee5da5e
Melanson <Lee.Melanson@LibertyMutual.com>
2022-09-19 20:00:28 -0400

    connect to parks json file

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,28 @@
-console.log("js loaded");
-fetch('https://server.com/data.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+    async function getParks() {
+        let url = 'https://uptacamp.github.io/session_two/assets/scripts/data/national_parks.json';
+        try {
+            let res = await fetch(url);
+            return await res.json();
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    async function displayParks() {
+        let parks_result = await getParks();
+        
+        for (let i = 0; i < parks_result.parks.length; i++){
+            console.log(parks_result.parks[i].Address);
+        }
+        
+        //return parks_result.parks[0].Address;
+        }
+    
+        
+    
+   displayParks();
+    
+
+
+    
