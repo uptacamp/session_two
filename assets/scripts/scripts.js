c91ff789f5dabf1eda7e5b925ba602ad5252c840
Melanson <Lee.Melanson@LibertyMutual.com>
2022-09-22 15:02:15 -0400

    add new json

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,8 +1,8 @@
     
     
     let parks_result = {};
-
-
+  
+    //retrieve parks json file - the primary file driving this page
     async function getParks() {
         let url = 'https://uptacamp.github.io/session_two/assets/scripts/data/national_parks.json';
         try {
@@ -14,13 +14,13 @@
         }
     }
     
-    async function buildParksDropDown() {
+    //from the parks json file, build the locations drop down
+    async function buildLocationsDropDown() {
         parks_result = await getParks();      
         let state_list = [];
         let location_dropdown = $('#by_location');
-        
         location_dropdown.empty();
-        location_dropdown.append('<option selected="true"> Choose State</option>');
+        location_dropdown.append('<option selected="true">All</option>');
         location_dropdown.prop('selectedIndex', 0);
         
         //cycle through json file to build a sorted drop down list with no dupes
@@ -35,25 +35,47 @@
                 state_list.sort();
                 }
         }
-
+        
         state_list.forEach(addToDropDown)
 
         function addToDropDown(item) {
             location_dropdown.append($('<option></option>').attr('value', item).text(item));
          } 
-       console.log(state_list);
+       //console.log(state_list);
         }
-    
 
-        function showResults(){
-            location_dropdown = $('#by_location');
-            if (location_dropdown.text == "Choose State") {
-                console.log("nothing selected)");
-            } else {
-                console.log(location_dropdown.val());
-            }
-    
+    //retrieve park types json file - the secondary file driving this page
+    async function getParkTypes() {
+        let url = 'https://uptacamp.github.io/session_two/assets/scripts/data/park_types.json';
+        try {
+            let res2 = fetch(url);
+            JSON.Parse(res2);
+            console.log(res2);
+            return await res2.json();
+                        
+        } catch (error) {
+            console.log(error);
         }
+    }
+
+    async function buildParkTypesDropDown() {
+        parks_result = await getParkTypes();      
+        let type_list = [];
+        console.log(parks_result)
+        let type_dropdown = $('#by_type');
+        type_dropdown.empty();
+        type_dropdown.append('<option selected="true">All</option>');
+        type_dropdown.prop('selectedIndex', 0);
+               
+        type_list.forEach(addToDropDown)
+
+        function addToDropDown(item) {
+            type_dropdown.append($('<option></option>').attr('value', item).text(item));
+         } 
+     
+        }
+
+
 
 
         function DisplayFilteredParksList (byLocation, byType){
@@ -69,30 +91,21 @@
                 if (current_state == query_state){
                     //add to results array
                     matching_results.push(parks_result.parks[i]);
-                    
-                  
-
-                    
-                    
-
-                } else {
-                    //move on   
-                }
-               
+                } 
             }
             //display matching results on screen
             console.log(matching_results);
             for (n = 0; n < matching_results.length; n++){
                 $('#parks_results_table').append('<tr><td>' + matching_results[n].LocationName + ' ' + '</td> <td>' + matching_results[n].City + ' ' + '</td> <td>'+ matching_results[n].State + '</td></tr>');
             }
-
-           // $('#parks_results_table').html('<tr><td>' + matching_results[0].LocationName + ' ' + '</td> <td>' + matching_results[0].City + ' ' + '</td> <td>'+ matching_results[0].State + '</td></tr>');
-
         }
 
         
 
-    buildParksDropDown();
+    //load the page dropdowns
+    buildLocationsDropDown();
+    getParkTypes();
+    // add when ready>buildParkTypeDropDown();
     
     
 
